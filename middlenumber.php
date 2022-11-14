691c29eecbb8fbea2a01502bff82fc37a2dab63d
saradabohara <117718800+saradabohara@users.noreply.github.com>
2022-11-14 11:00:48 +0545

    commit

--- middlenumber.php
+++ middlenumber.php
@@ -1,7 +1,8 @@
 <?php
 $numbers=array(2,5,7,8,9,13,18,20);
 foreach($numbers as $value){
-    if($value<10 && $value>5){
+    //condition for numbers between 5 and 10 
+    if($value<10 && $value>5){  
         echo $value;
         echo "<br>";
     }
