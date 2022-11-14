913267dcf216a2e1f4af223ca3bbf335d274b0e0
saradabohara <117718800+saradabohara@users.noreply.github.com>
2022-11-14 11:41:52 +0545

    commit

---
+++ onlyif.php
@@ -0,0 +1,23 @@
+<?php
+
+// for finding the given number is smaller or greater than 10
+$number=11;
+if($number<10){
+    echo $number;
+}
+else{
+    echo 'The number is greater than given value';
+}
+echo "<hr/>";
+?>
+<?php
+//for finding the given number lies betn 5 and 10 or not...
+
+$number=7;
+if($number>5 && $number<10){
+    echo "TRUE";
+
+}else{
+    echo "False";
+}
+?>
